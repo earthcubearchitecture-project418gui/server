75c39cc73e49eaed91d0f9760ef0f230e926126d
Alex M <amcnurlan@gmail.com>
2018-11-02 13:10:03 -0400

    Added process.env.PORT for heroku"

--- src/app-local-express.js
+++ src/app-local-express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 const { validateJSON } = require('./validateJSON');
 const { datasetPrimativesValidator } = require('./js_templates/dataset_template_primatives');
